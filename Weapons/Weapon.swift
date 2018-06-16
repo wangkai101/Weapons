3271648305567c152e9f0a9f3471b87090ff91e0
wangkai101 <645182907@qq.com>
2018-06-16 14:03:32 +0800

    model重构 有一个警告

---
+++ Weapons/Weapon.swift
@@ -0,0 +1,13 @@
+struct Weapon {
+    var name = ""
+    var type = ""
+    var origin = ""
+    var image = ""
+    var favourite = false
+    
+    var bullet = 0.0
+    var mv = 0
+    var text = ""
+}
+
+
